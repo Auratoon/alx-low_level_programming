78d171c18f45ca028daac75983fcab709b7cd23a
aura <aura.rhln@gmail.com>
2023-12-25 08:14:30 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal -  draws a diagonal line on the terminal
+ * @n:  the number of times the character \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int l, c;
+
+	if (n > 0)
+	{
+		for (l = 0; l <= n; l++)
+		{
+			for (c = 0; c < l; c++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
