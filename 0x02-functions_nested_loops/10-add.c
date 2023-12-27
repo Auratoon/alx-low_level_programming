07b535d48a117d81172282c275a1d4e8b064dc52
aura <aura.rhln@gmail.com>
2023-12-27 08:16:05 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
