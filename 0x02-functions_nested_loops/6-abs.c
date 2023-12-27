8b7ad1af5fc9c4ba858175001cfa1897ef9b628b
aura <aura.rhln@gmail.com>
2023-12-27 05:30:18 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: input
+ *
+ * Return: the absolute value of the integer argument
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
