66885fec20f9a29e6548f9d311931662ff3d0f0b
aura <aura.rhln@gmail.com>
2023-12-28 06:08:27 -0800

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table
+ * @n: input
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (i * j < 10)
+					_putchar('0' + i * j);
+				else if (i * j >= 10 && i * j < 100)
+				{
+					_putchar('0' + i * j / 10);
+					_putchar('0' + i * j % 10);
+				}
+				else
+				{
+					_putchar('0' + i * j / 100);
+					_putchar('0' + (i * j % 100) / 10);
+					_putchar('0' + i * j % 10);
+				}
+				if (j != n)
+				{
+					if (i * j < 10 && i * (j + 1) < 10)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (i * j < 100 && i * (j + 1) < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
