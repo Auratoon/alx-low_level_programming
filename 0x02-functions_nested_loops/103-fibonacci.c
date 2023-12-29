a2ab9728782e52975abdd758775160ca1eadfaf8
aura <aura.rhln@gmail.com>
2023-12-29 05:26:01 -0800

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int a, b, c, sum;
+
+	sum = 0;
+	a = 0;
+	b = 1;
+	while (a + b < 4000000)
+	{
+		if (a + b % 2 == 0)
+			sum += a + b;
+		c = b;
+		b = a + b;
+		a = c;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
