c330b5fb26d5534e889131b4703b21b526d9c032
aura <aura.rhln@gmail.com>
2023-12-29 05:14:34 -0800

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -  prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	long int a, b, c;
+
+	count = 0;
+	a = 0;
+	b = 1;
+	while (count < 50)
+	{
+		printf("%d, ", a + b);
+		c = b;
+		b = a + b;
+		a = c;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
