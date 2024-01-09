0a5988a1949b4a5b5ef17a166265a1c2f96e9b97
aura <aura.rhln@gmail.com>
2024-01-09 07:46:54 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer as parameter and updates the value to 98
- * @n: pointer to update
+ * reset_to_98 -  takes a pointer to an int and updates the value to 98
+ * @n: parameter
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
