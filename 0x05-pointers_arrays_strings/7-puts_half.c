cf04c64cce010141f4a22495d1d8c5225f9ba392
aura <aura.rhln@gmail.com>
2024-01-10 06:00:24 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int i, len = 0;
+
+	while(str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+	{
+		for (i = 0; i <= (len - 1) / 2; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (len + 1) / 2; i < len; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
