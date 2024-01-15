93c2a40ddff8285e7c84f7e3093e4cf5f843db20
aura <aura.rhln@gmail.com>
2024-01-15 06:15:14 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, diff = 0;
+	int i = 0, diff = 0;
 
 	do {
-		diff = s1[i] - s2[j];
+		diff = s1[i] - s2[i];
 		i++;
-		j++;
-	} while (s1[i] == s2[j] && s1[i] != '\0' && s2[j] != '\0');
+	} while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0');
 	return (diff);
 }
