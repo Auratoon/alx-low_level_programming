733cba02eff984329a8ab81efcbfef03b7d85a67
aura <aura.rhln@gmail.com>
2024-01-15 04:45:49 -0800

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strncat -  concatenates two strings, using at most n bytes from src
+ * @dest: string to append to
+ * @src: string to append
+ * @n: number of bytes
+ *
+ * Return: a pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, len = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (i < n && src[i] != '\0')
+		dest[len + i] = src[i];
+	dest[len + i] = '\0';
+	resturn (dest);
+}
