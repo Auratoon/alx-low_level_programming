cc7b7042a9af6336244b95cb144a93a41c828e3f
aura <aura.rhln@gmail.com>
2024-01-15 05:02:08 -0800

    Tsk 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,10 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[len] != '\0')
 		len++;
 	while (i < n && src[i] != '\0')
+	{
 		dest[len + i] = src[i];
+		i++;
+	}
 	dest[len + i] = '\0';
 	return (dest);
 }
